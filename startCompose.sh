00b8b94768043301cc36536a1f557ed70fc688cc
Michael Kahn <Michael.Kahn@cuanschutz.edu>
2022-11-02 13:58:57 -0600

    Remove extraneous YML configs; new embedded YML script using echo command rather than EOF

--- startCompose.sh
+++ startCompose.sh
@@ -6,6 +6,7 @@ USER_ID=`id -u` USER_NAME=`id -n -u` docker-compose pull
 USER_ID=`id -u` USER_NAME=`id -n -u` docker-compose up  \
   --abort-on-container-exit \
   --exit-code-from ui & 
+echo "Sleeping 5 seconds to allow startup"
 sleep 5   
 open http://localhost:6080 \
   || xdg-open http://localhost:6080 \
