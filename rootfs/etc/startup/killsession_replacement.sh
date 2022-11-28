0332371bb4ce78e2671cdb4f2f726caad2de458c
Michael Kahn <Michael.Kahn@cuanschutz.edu>
2022-11-28 08:49:12 -0700

    Replacement killsession.py with new ExitSession message

---
+++ rootfs/etc/startup/killsession_replacement.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Replace original killsession.py with my version
+
+# V1.0: Alter Exit Session message text
+
+cp /etc/startup/killsession/killsession.py /usr/local/bin/killsession.py
